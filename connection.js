1f85064908d611d39d671ccfe73fe8184e390ce4
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-03-06 23:19:34 +0530

    Add MongoDB connection code

---
+++ connection.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+async function connectToMongoDB(url) {
+    try {
+        await mongoose.connect(url);
+        console.log("MongoDB connected successfully");
+    } catch (error) {
+        console.log("Failed to connect to MongoDB:", error);
+        process.exit(1);
+    }
+}
+
+module.exports = {
+    connectToMongoDB
+}
--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
-const dotenv = require('dotenv').config();
+require('dotenv').config();
+const { connectToMongoDB } = require("./connection.js");
 
 const app = express();
 const PORT = process.env.PORT || 8001;
 
+
+// Connect to MongoDB
+connectToMongoDB(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("MongoDB connected successfully");
+    })
+    .catch((err) => {
+        console.log("Failed to connect to MongoDB:", err);
+        process.exit(1);
+    })
+
+    
 app.listen(PORT, () => {
     console.log(`Server is started at port: ${PORT}`);
 });
