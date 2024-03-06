dacc8fc4a93b6bde77c5627441518131028b5925
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-03-06 23:30:40 +0530

    Refactor connectToMongoDB function

--- connection.js
+++ connection.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 async function connectToMongoDB(url) {
     try {
         await mongoose.connect(url);
-        console.log("MongoDB connected successfully");
     } catch (error) {
         console.log("Failed to connect to MongoDB:", error);
         process.exit(1);
