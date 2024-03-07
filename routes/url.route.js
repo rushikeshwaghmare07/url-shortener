68b9eb664bf636e34ed4156a7050a584a739aac1
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-03-07 12:09:47 +0530

    Add route definitions

---
+++ routes/url.route.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router()
+
+// Route for generating short URL
+router.post("/", );
+
+// Route for redirecting short URL to original URL
+router.get("/shortId", )
+
+// Routes for getting analytics
+router.get("/analytics/:shortId", );
+
+module.exports = router;
